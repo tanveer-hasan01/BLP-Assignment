f7508aca8af136e4d9906c15fc2c937a2fa1e8f2
tanveer <tanveer.hasan011@gmail.com>
2023-07-21 11:47:21 +0600

    done recycler View

--- app/src/main/java/com/example/blp_task/DetailsActivity.kt
+++ app/src/main/java/com/example/blp_task/DetailsActivity.kt
@@ -4,13 +4,16 @@ import android.annotation.SuppressLint
 import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Adapter
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import android.widget.Toolbar
 import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.blp_task.R.id.back
 import com.example.blp_task.R.id.my_toolbar
+import com.example.blp_task.adapter.ProductsAdapter
 import com.example.blp_task.databinding.ActivityDetailsBinding
 import com.example.blp_task.databinding.MyToobarBinding
 import com.example.blp_task.dataclass.ProductsItem
@@ -25,6 +28,8 @@ class DetailsActivity : AppCompatActivity() {
 
     private lateinit var bindingToolbar: MyToobarBinding
     private lateinit var binding: ActivityDetailsBinding
+    lateinit var adapter: ProductsAdapter
+    lateinit var linearLayoutManager: LinearLayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,33 +39,44 @@ class DetailsActivity : AppCompatActivity() {
 
         //toobar
         setSupportActionBar(binding.toobar.myToolbar)
-        binding.toobar.back.isVisible=true
-        binding.toobar.title.text=intent.getStringExtra("category")
+        binding.toobar.back.isVisible = true
+        binding.toobar.title.text = intent.getStringExtra("category")
         binding.toobar.back.setOnClickListener { onBackPressedDispatcher.onBackPressed() }
         binding.toobar.notification.setOnClickListener {
-            Toast.makeText(this,"Not Implemented..",Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Not Implemented..", Toast.LENGTH_LONG).show()
         }
 
         binding.cardView.setBackgroundResource(R.drawable.sergel);
 
 
-        //call api
-        val medApi = RetrofitInstance.getInstance().create(ApiInterface::class.java)
+        linearLayoutManager = LinearLayoutManager(this)
+        binding.productRecycler.layoutManager = linearLayoutManager
 
+        //call api
+        getProducts()
+
+
+    }
+
+
+    private fun getProducts() {
+        val medApi = RetrofitInstance.getInstance().create(ApiInterface::class.java)
         medApi.getProducts().enqueue(object : Callback<List<ProductsItem>?> {
             override fun onResponse(
                 call: Call<List<ProductsItem>?>,
                 response: Response<List<ProductsItem>?>
             ) {
-                Snackbar.make(binding.root, ""+response.body()!!.size, Snackbar.LENGTH_SHORT).show()
+                val responseBody = response.body()
+                adapter = ProductsAdapter(baseContext, responseBody!!)
+                adapter.notifyDataSetChanged()
+                binding.productRecycler.adapter = adapter
+                binding.progressBar.isVisible = false
             }
 
             override fun onFailure(call: Call<List<ProductsItem>?>, t: Throwable) {
-
+                binding.progressBar.isVisible = false
                 Snackbar.make(binding.root, "Data not found", Snackbar.LENGTH_SHORT).show()
             }
         })
-
-
     }
 }
---
+++ app/src/main/java/com/example/blp_task/adapter/ProductsAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.blp_task.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.blp_task.databinding.ItemRecyclerviewBinding
+import com.example.blp_task.dataclass.ProductsItem
+
+class ProductsAdapter(val context: Context,val productList: List<ProductsItem>): RecyclerView.Adapter<ProductsAdapter.ViewHolder>() {
+
+
+    inner class ViewHolder(val binding: ItemRecyclerviewBinding)
+        :RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductsAdapter.ViewHolder {
+        val binding = ItemRecyclerviewBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ProductsAdapter.ViewHolder, position: Int) {
+
+        holder.binding.title.text= productList[position].title
+        holder.binding.subtitle.text= productList[position].subtitle
+        holder.binding.date.text= productList[position].create_date
+
+        Glide.with(holder.itemView)
+            .load(productList[position].image).fitCenter()
+            .into(holder.binding.medImage)
+
+
+
+    }
+
+    override fun getItemCount(): Int {
+      return productList.size
+    }
+}
